951102e8b7a84c494f106e08842c9e2a23e57346
seha1 <seha1@yandex.ru>
2021-02-18 20:28:44 +0300

    Главная страница где происходит все непотребство. можно потрогать

---
+++ src/Components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Main extends React.Component{
+    constructor() {
+        super();
+    }
+
+    render(){
+        return(
+            <div>1</div>
+        )
+    }
+}
+
+export default Main;
+
