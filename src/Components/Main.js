a7fb52c4a46ee37ac84495af31409b46c82b06d5
seha1 <seha1@yandex.ru>
2021-02-18 23:05:44 +0300

    Разметка игрового поля. Из-за трабла вывода box-sazing в Opera сверстоно на floatах вместо floatов. Хотя проблему это не решило. да здоавствуют танцы с бубном. вернемся к этому позже. может гриды? Отличное начало.

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import Style from './main.module.css';
 
 
-class Main extends React.Component{
+class Main extends React.Component {
     constructor() {
         super();
     }
 
-    render(){
-        return(
-            <div>1</div>
+    render() {
+        return (
+            <div className={Style.main}>
+                <div className={Style.wrapper}>
+                    <div className={Style.playingField}>
+                <div className={Style.squareItem} id = '1' data = '1'>1</div>
+                <div className={Style.squareItem} id = '2' data = '2'>2</div>
+                <div className={Style.squareItem} id = '3' data = '3'>3</div>
+                <div className={Style.squareItem} id = '4' data = '4'>4</div>
+                <div className={Style.squareItem} id = '5' data = '5'>5</div>
+                <div className={Style.squareItem} id = '6' data = '6'>6</div>
+                <div className={Style.squareItem} id = '7' data = '7'>7</div>
+                <div className={Style.squareItem} id = '8' data = '8'>8</div>
+                <div className={Style.squareItem} id = '9' data = '9'>9</div>
+
+                    </div>
+                </div>
+            </div>
         )
     }
 }
